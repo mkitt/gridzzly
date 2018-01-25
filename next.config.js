0caa5e52cccb88f86a6b8123aa2c34e826657409
Matthew Kitt <mk@mkitt.net>
2018-01-25 03:16:25 -0700

    Add the cdn asset prefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
+  assetPrefix: isProd ? 'http://mkitt.net/gridzzly' : '',
   exportPathMap() {
     return {
       '/': { page: '/' },
