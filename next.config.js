33af49a78948e421fded23816e9df2ae664149c6
Matthew Kitt <mk@mkitt.net>
2018-02-06 05:44:26 -0700

    Use https for assets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-  assetPrefix: isProd ? 'http://mkitt.net/gridzzly' : '',
+  assetPrefix: isProd ? 'https://mkitt.net/gridzzly' : '',
   exportPathMap() {
     return {
       '/': { page: '/' },
